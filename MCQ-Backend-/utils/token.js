4b44667aeb22e0cf12e894abbdeea79757bb4ea8
Yashvardhandhondge <yashvardhandhondge@gmail.com>
2025-12-20 13:45:23 +0530

    update

--- MCQ-Backend-/utils/token.js
+++ MCQ-Backend-/utils/token.js
@@ -7,7 +7,7 @@ const signToken = (payload, expiresIn = process.env.JWT_EXPIRES_IN || '7d') => {
 
   return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn });
 };
-
+//
 const verifyToken = (token) => {
   if (!process.env.JWT_SECRET) {
     throw new Error('JWT_SECRET is not defined');
