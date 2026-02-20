95dd42e82f060f05a940d2fd3c5ac19b9555af0a
Yashvadhandhondge <yashvardhandhondge@gmail.com>
2026-02-20 10:37:03 +0530

    updated config

--- my-app/lib/auth.ts
+++ my-app/lib/auth.ts
@@ -1,5 +1,5 @@
-const API_URL = 'http://localhost:8000';
-// const API_URL='https://goldfish-app-vwvh7.ondigitalocean.app';
+// const API_URL = 'http://localhost:8000';
+const API_URL='https://goldfish-app-vwvh7.ondigitalocean.app';
 
 
 export interface User {
