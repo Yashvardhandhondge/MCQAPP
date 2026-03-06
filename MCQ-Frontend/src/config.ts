a74b85619965f04f207d037abe0f206c1666c0ba
Yashvadhandhondge <yashvardhandhondge@gmail.com>
2026-03-07 00:20:29 +0530

    update

--- MCQ-Frontend/src/config.ts
+++ MCQ-Frontend/src/config.ts
@@ -10,12 +10,12 @@ let API_BASE_URL: string;
 const ENV_API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL?.trim();
 
 if (__DEV__) {
-  if (ENV_API_BASE_URL) {
+if (ENV_API_BASE_URL) {
     API_BASE_URL = ENV_API_BASE_URL;
   } else if (Platform.OS === 'android') {
     // Android Emulator default for local backend
-    API_BASE_URL = 'http://192.168.29.158:8000';
-    // API_BASE_URL = 'https://goldfish-app-vwvh7.ondigitalocean.app';
+    // API_BASE_URL = 'http://192.168.29.158:8000';
+    API_BASE_URL = 'https://goldfish-app-vwvh7.ondigitalocean.app';
   } else {
     // iOS Simulator default for local backend
     // API_BASE_URL = 'http://localhost:8000';
@@ -30,9 +30,9 @@ if (__DEV__) {
     // API_BASE_URL ='https://mcqapp-nu.vercel.app';
     // API_BASE_URL = 'http://192.168.29.132:8000';
     // API_BASE_URL = 'http://10.228.232.180:8000';
-    API_BASE_URL = 'http://192.168.29.158:8000'; // Local URL - commented out
+    // API_BASE_URL = 'http://192.168.29.158:8000'; // Local URL - commented out
     // API_BASE_URL = 'http://10.228.232.180:8000';
-    // API_BASE_URL = 'https://goldfish-app-vwvh7.ondigitalocean.app';
+    API_BASE_URL = 'https://goldfish-app-vwvh7.ondigitalocean.app';
     // API_BASE_URL = 'http://192.168.1.6:8000'; // Local URL - commented out
 
 
